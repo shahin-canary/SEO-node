cd4bd82411dd3692f457a48bf05d2e2e3d5858c0
shahin-canary <shahincanary@gmail.com>
2025-02-24 12:37:08 +0530

    first commit-

--- index.js
+++ index.js
@@ -232,7 +232,7 @@ app.post("/audit", async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
